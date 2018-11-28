0aa6f4c22f6d39c06aeba43adb340910196f2f03
Daniel Eguia <candelario.eguia@laverne.edu>
2018-11-27 22:21:26 -0800

    Formating; + speed options

--- main.cpp
+++ main.cpp
@@ -13,44 +13,42 @@ typedef struct{
   int live;
 }Code;
 
-void render(deque<map<int, Code> > &matrix);
+void render(deque<map<int, Code> > &matrix, int alphaSize);
 
-void usage(void) {
-  printf(" Usage: cmatrix -[h]\n"); // "[abBfhlsVx] [-u delay] [-C color]" to be added
-  printf(" -h: Prints this help menu\n");
-}
+void usage(void); 
 
-int msleep(unsigned long milisec)
-{
-    struct timespec req={0};
-    int speedAdjustment = 1000; // slow: 100; fast: 250; default: 1000
-    time_t sec=(int)(milisec/speedAdjustment);
-    milisec=milisec-(sec*speedAdjustment);
-    req.tv_sec=sec;
-    req.tv_nsec=milisec*speedAdjustment*1000L;
-    while(nanosleep(&req,NULL)==-1)
-        continue;
-    return 1;
-}
+int msleep(unsigned long milisec);
 
-int main(int argc, char **argv){
+int main(int argc, char *argv[]){
   static const char alpha[] =
-    "0123456789"
-    "!@#$%^&*"
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  int alphaSize = sizeof(alpha)/sizeof(char);
+      "0123456789"
+      "!@#$%^&*"
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  int alphaSize = sizeof(alpha) / sizeof(char);
   //cout<<alphaSize;
   int optchr;
+  int speed = 5; // default fall speed.
 
-/* Get Options */
-while ((optchr = getopt(argc, argv, "abBfhlLnrosxVu:C:")) != EOF) {
-  switch (optchr) {
+  /* Get Options */
+  while ((optchr = getopt(argc, argv, "abBfhlLnroxVs:u:C:")) != EOF)
+  {
+    switch (optchr)
+    {
     case 'h':
     case '?':
-    usage();
-    exit(0);
+      usage();
+      exit(0);
+    case 's':
+      int input = atoi(optarg);
+      if (0 <= input && input <= 10){
+        speed = atoi(optarg);
+        break;
+      } else{
+        printf("Input out of range.\n");
+        exit(0);
+      }
+    }
   }
-}
 
   // get terminal size
   struct winsize w;
@@ -58,49 +56,79 @@ while ((optchr = getopt(argc, argv, "abBfhlLnrosxVu:C:")) != EOF) {
   int col_size = w.ws_col;
   int row_size = w.ws_row;
   //cout<< col_size<< " "<<row_size<<endl;
-  
-  deque<map<int, Code> > matrix;
-  matrix.push_front(map<int,Code>());
-  for(;;){
-    map<int,Code> new_map = matrix[0];
-    int new_col = rand()%col_size+1;
-    int new_live = rand()%row_size+1;
-    Code empty_code = {'0',new_live};
+
+  deque<map<int, Code>> matrix;
+  matrix.push_front(map<int, Code>());
+  for (;;)
+  {
+    map<int, Code> new_map = matrix[0];
+    int new_col = rand() % col_size + 1;  // set column size proportional to screen height
+    int new_live = rand() % row_size + 1;
+    Code empty_code = {'0', new_live};
     new_map[new_col] = empty_code;
-    for(map<int,Code>::iterator it = new_map.begin();it!=new_map.end();){
-      int new_c = alpha[rand()%alphaSize];
+    
+    for (map<int, Code>::iterator it = new_map.begin(); it != new_map.end();){
+      int new_c = alpha[rand() % alphaSize];
       it->second.c = new_c;
       it->second.live--;
       // clear 0 live code
-      if(it->second.live==0){
+      if (it->second.live == 0)
+      {
         new_map.erase(it);
-      }else{
+      }
+      else
+      {
         it++;
       }
     }
     matrix.push_front(new_map);
-    // clear row that out of terminal
-    if(matrix.size()>row_size){
-      matrix.erase(matrix.begin()+row_size,matrix.end());
+
+    // clear row thats out of terminal
+    if (matrix.size() > row_size)
+    {
+      matrix.erase(matrix.begin() + row_size, matrix.end());
     }
-    render(matrix);
+
+    render(matrix, speed);
   }
   msleep(1000L);
   return 0;
 }
 
-void render(deque<map<int, Code> > &matrix){
+void render(deque<map<int, Code> > &matrix, int speed){
   // clear screen
   printf("\033[2J\033[1;1H");
   // render
-  for(int i = 0;i<matrix.size();i++){
-    for(map<int,Code>::iterator it = matrix[i].begin();it!=matrix[i].end();it++){
+  for(int i = 0; i<matrix.size(); i++){
+    for(map<int,Code>::iterator it = matrix[i].begin(); it!=matrix[i].end(); it++){
       int print_col = it->first;
       Code print_code = it->second;
-      printf("\033[%d;%dH",i,print_col);//, 0, print_col);
-      printf("%c",print_code.c);
+//     if (it == matrix[i].begin()){
+//           printf("\033[%d;%dH",i,print_col);//, 0, print_col);
+//            printf("%c\n", rand()%sizeof(char));      
+//      }else{
+        printf("\033[%d;%dH",i,print_col);//, 0, print_col);      
+        printf("%c",print_code.c);
+//      }
     }
   }
   fflush(stdout);
-  msleep(100);
+  msleep(speed*20);
+}
+
+void usage(void) {
+  printf(" Usage: cmatrix -[h] [-s speed]\n"); // "[abBfhlsVx] [-u delay] [-C color]" to be added
+  printf(" -h: Prints this help menu\n");
+  printf(" -s: delay (0 - 200, default 100): Screen update speed \n");
+}
+
+int msleep(unsigned long milisec){
+  struct timespec req = {0};
+  time_t sec = (int)(milisec / 1000);
+  milisec = milisec - (sec * 1000);
+  req.tv_sec = sec;
+  req.tv_nsec = milisec * 1000000L;
+  while (nanosleep(&req, NULL) == -1)
+    continue;
+  return 1;
 }
